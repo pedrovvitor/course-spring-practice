949a99b731d4e2e37ba0c52b623105ee91f55cb0
Pedro Lima <pedrojppb@gmail.com>
2020-04-08 18:15:44 -0300

    Primeiro commit: Projeto criado

---
+++ src/main/java/com/pedrolima/springrest/CoursespringrestApplication.java
@@ -0,0 +1,13 @@
+package com.pedrolima.springrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursespringrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursespringrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedrolima/springrest/CoursespringrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrolima.springrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursespringrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
