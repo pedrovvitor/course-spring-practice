be9e02f96e41e7cad8aac66c76beea33bc5b72be
Pedro Lima <pedrojppb@gmail.com>
2020-04-08 22:07:55 -0300

    Testanto a primeira classe de dominio

--- src/main/java/com/pedrolima/springrest/resources/CategoryResource.java
+++ src/main/java/com/pedrolima/springrest/resources/CategoryResource.java
@@ -1,16 +1,20 @@
 package com.pedrolima.springrest.resources;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pedrolima.springrest.entities.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
 	@GetMapping
-	public String test() {
-		return "REST is working";
+	public List<Category> findAll() {
+		return null;
 	}
 
 }
