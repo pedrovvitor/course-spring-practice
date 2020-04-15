3a964d894976b3f96b8cec13cbea52d262c20d49
Pedro Lima <pedrojppb@gmail.com>
2020-04-15 18:19:49 -0300

    Listando todas Categorias com DTO

---
+++ src/main/java/com/pedrolima/springrest/dto/CategoryDTO.java
@@ -0,0 +1,37 @@
+package com.pedrolima.springrest.dto;
+
+import java.io.Serializable;
+
+import com.pedrolima.springrest.entities.Category;
+
+public class CategoryDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String name;
+
+	public CategoryDTO() {
+	}
+	
+	public CategoryDTO(Category obj) {
+		id = obj.getId();
+		name = obj.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
--- src/main/java/com/pedrolima/springrest/resources/CategoryResource.java
+++ src/main/java/com/pedrolima/springrest/resources/CategoryResource.java
@@ -2,6 +2,7 @@ package com.pedrolima.springrest.resources;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.pedrolima.springrest.dto.CategoryDTO;
 import com.pedrolima.springrest.entities.Category;
 import com.pedrolima.springrest.services.CategoryService;
 
@@ -26,9 +28,10 @@ public class CategoryResource {
 	private CategoryService service;
 
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> categories = service.findAll();
-		return ResponseEntity.ok().body(categories);
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<Category> list = service.findAll();
+		List<CategoryDTO> listDto = list.stream().map(obj -> new CategoryDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 	@GetMapping(value = "/{id}")
