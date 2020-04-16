aa5c4a3231a5cbe8f5e9dbd4e374bf03fc03b8fe
Pedro Lima <pedrojppb@gmail.com>
2020-04-16 11:58:10 -0300

    Personalizando o retorno da validação

---
+++ src/main/java/com/pedrolima/springrest/resources/exceptions/ValidationError.java
@@ -0,0 +1,23 @@
+package com.pedrolima.springrest.resources.exceptions;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<FieldMessage>();
+
+	public ValidationError(Integer status, String msg, Instant instant) {
+		super(status, msg, instant);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+}
