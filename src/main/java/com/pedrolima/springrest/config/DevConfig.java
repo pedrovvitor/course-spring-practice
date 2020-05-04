ee9531514339ed519db20fafc4d000477151b868
Pedro Lima <pedrojppb@gmail.com>
2020-05-04 13:06:33 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/pedrolima/springrest/config/DevConfig.java
+++ src/main/java/com/pedrolima/springrest/config/DevConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.pedrolima.springrest.services.DBService;
+import com.pedrolima.springrest.services.EmailService;
+import com.pedrolima.springrest.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -26,5 +28,9 @@ public class DevConfig {
 		dbService.instantiateDatabase();
 		return true;
 	}
-
+	
+	@Bean
+	public EmailService emailService()	{
+		return new SmtpEmailService();
+	}
 }
