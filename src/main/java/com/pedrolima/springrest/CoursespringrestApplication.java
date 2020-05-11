9f735527ab01f4c5c0a18dde9f9d14b4cd421639
Pedro Lima <pedrojppb@gmail.com>
2020-05-11 20:12:07 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/pedrolima/springrest/CoursespringrestApplication.java
+++ src/main/java/com/pedrolima/springrest/CoursespringrestApplication.java
@@ -1,13 +1,24 @@
 package com.pedrolima.springrest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.pedrolima.springrest.services.S3Service;
+
 
 @SpringBootApplication
-public class CoursespringrestApplication {
+public class CoursespringrestApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoursespringrestApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {	
+		s3Service.uploadFile("C:\\Users\\pedro\\OneDrive\\Pictures\\cadastroOk.png");
+	}
 }
