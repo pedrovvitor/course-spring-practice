eff2008e56888e55f97ee9a228b28a75c1abe5f4
Pedro Lima <pedrojppb@gmail.com>
2020-05-12 09:32:44 -0300

    Ajustando o tamanho da imagem

--- src/main/java/com/pedrolima/springrest/services/CustomerService.java
+++ src/main/java/com/pedrolima/springrest/services/CustomerService.java
@@ -46,6 +46,10 @@ public class CustomerService {
 	("${img.prefix.client.profile}")
 	private String prefix;
 	
+	@Value
+	("${img.profile.size}")
+	private Integer size;
+	
 	public Customer findById(Long id) {
 		
 		UserSS user = UserService.authenticated();
@@ -117,6 +121,8 @@ public class CustomerService {
 		}
 		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
 		String fileName = prefix + user.getId() + ".jpg";
 		
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
